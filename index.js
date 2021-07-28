1da59680be8cb8deaee5d930f2739ed45bb34478
Satyabrat Panda <46991649+satyap54@users.noreply.github.com>
2021-07-28 15:29:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ const mongoose = require('mongoose');
 
 
 try{
-	mongoose.connect(
-		process.env["MONGO_URI"],
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			serverSelectionTimeoutMS: 5000
-		}
-	)
+  mongoose.connect(
+    process.env["MONGO_URI"],
+      {
+        useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverSelectionTimeoutMS: 5000
+      }
+  )
 }catch(error){
-	console.error(error);
+  console.error(error);
 }
 
 
